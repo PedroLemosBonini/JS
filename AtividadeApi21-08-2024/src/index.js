d9eefb0669285eca3c3c1354f34b1aa9f162ba62
pedrolemosbonini <pedrolemosbonini@gmail.com>
2024-08-28 13:57:50 -0300

    AtividadeAPI28-08-2024

--- AtividadeApi21-08-2024/src/index.js
+++ AtividadeApi21-08-2024/src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 // Define uma classe para organizar a lógica da aplicação
 class AppController{
     constructor(){
+
         // Cria uma nova instância do Express dentro da classe
         this.express = express();
         // Chama o método middlewares para configurar os middlewares
@@ -17,6 +18,52 @@ class AppController{
 
     // Define as rotas da nossa API
     routes(){
+        const users = [];
+
+        this.express.post("/users",(req,res)=>{
+            const {id,nome,email,senha} = req.body
+            users.push({ id,nome,email,senha });
+            res.status(200).send({ message:"Usuário Cadastrado com Sucesso" })
+        });
+
+        this.express.post("/auth",(req,res)=>{
+            const {email,senha} = req.body
+
+            const user_email = users.find((user_email) => user_email.email == email);
+
+            if(user_email){
+                const user_senha = users.find((user_senha) => user_senha.senha == senha);
+                if(user_senha){
+                    res.status(200).send({ message: "Usuário Autenticado"})
+                }
+                else{
+                    res.status(400).send({ message: "Usuário Não Encontrado"})
+                }
+            }
+            else{
+                res.status(400).send({ message: "Usuário Não Encontrado" })
+            }
+
+            //res.status(200).send({ message:"Usuário Cadastrado com Sucesso" })
+        });
+        
+
+        this.express.get("/users/:id",(req,res)=>{
+            const{id} = req.params;
+            const user = users.find((user) => user.id == id);
+            if(user){
+                res.status(200).send(user)
+            }
+            else{
+                res.status(400).send({ message: "Usuário Não Encontrado" })
+            }
+        })
+
+
+
+
+
+
         // Define uma rota GET para o caminho health
         this.express.get('/health/',(req, res)=>{
             res.send({ nome: "Pedro Lemos Bonini", idade: "16", cpf: "45990449895" });
